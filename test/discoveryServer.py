86867ec05347affd5715277e040c4b1a1e4eadf2
Divyang Mittal <37113922+divyang-mittal@users.noreply.github.com>
2020-01-23 18:55:41 +0530

    Update discoveryServer.py

--- test/discoveryServer.py
+++ test/discoveryServer.py
@@ -1,4 +1,4 @@
 import serverModule
 
 if __name__ == "__main__":
-	discovery = serverModule.ServerModule(11111, 10)  // start server with port number and quality consideration index as parameters
+	discovery = serverModule.ServerModule(11111, 10)  # start server with port number and quality consideration index as parameters
